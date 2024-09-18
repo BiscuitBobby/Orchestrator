a85e50dc06c505c2403e4b514e608cdb002c0990
Siddharth Menon <simeddon@gmail.com>
2024-09-18 17:11:43 +0530

    Added vision tool utilizing gemini vision

--- functions/tools.py
+++ functions/tools.py
@@ -1,6 +1,7 @@
 from typing import Annotated, Literal
 from langchain_core.tools import tool
 from functions.discord_module.discord import DiscordMessage
+from functions.vision.cam import CamImgTool
 from functions.web.search import CustomSearch
 
 Operator = Literal["+", "-", "*", "/"]
@@ -20,7 +21,7 @@ def Calculator(operator: Annotated[Operator, "operator"], a:float, b:float) -> i
   else:
     raise ValueError("Invalid operator")
 
-langchain_tools = [CustomSearch,DiscordMessage]
+langchain_tools = [CustomSearch,DiscordMessage, CamImgTool]
 langchain_tools_description = '\n'.join([f"[{tool.name}]: {tool.description}" for tool in langchain_tools])
 print(langchain_tools)
 print(langchain_tools_description)
---
+++ functions/vision/cam.py
@@ -0,0 +1,47 @@
+from langchain_core.tools import tool
+from Secrets.keys import google_api
+import cv2
+import google.generativeai as genai
+import numpy as np
+from PIL import Image
+
+genai.configure(api_key=google_api)
+
+def describe_image(query):
+    cam = cv2.VideoCapture(0)
+    ret, image = cam.read()
+    cam.release()
+    if not ret:
+        raise Exception("Failed to capture image")
+    objects_description = recognize_objects(image, query)
+    return objects_description
+
+
+# Convert a numpy array to a PIL image
+def np_array_to_pil_image(image: np.ndarray) -> Image.Image:
+    return Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+
+# Recognize objects in the image using a generative model
+def recognize_objects(image: np.ndarray, query="What is in this image"):
+    pil_image = np_array_to_pil_image(image)
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+    prompt = query
+
+    cv2.namedWindow('Image', cv2.WINDOW_GUI_NORMAL)
+    cv2.imshow('Image', image)
+    cv2.waitKey(1 * 1000)
+
+    try:
+        response = model.generate_content([pil_image, prompt]).text
+    except Exception as e:
+        response = e
+    cv2.destroyAllWindows()
+    return response
+
+@tool(return_direct=True)
+def CamImgTool(query):
+    """Useful for recognizing objects, images and faces, using webcam or camera"""
+    print("Starting recognition")
+    response = describe_image(query)
+    return response
